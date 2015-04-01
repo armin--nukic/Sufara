6c5261711c658a996ba4e501bd077c789fe0e332
arminnukic <armin--nukic@hotmail.com>
2015-04-01 10:24:36 +0400

    armin commited

--- app/src/main/java/sufara/ba/edu/ibu/sufara/LoginActivity.java
+++ app/src/main/java/sufara/ba/edu/ibu/sufara/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
@@ -16,6 +18,15 @@ public class LoginActivity extends ActionBarActivity {
         setContentView(R.layout.activity_login);
 
         ImageView lekcije = (ImageView)findViewById(R.id.imageView);
+        Button prev = (Button)findViewById(R.id.button);
+        Button next = (Button)findViewById(R.id.button);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
 
